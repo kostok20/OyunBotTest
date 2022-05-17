236d6692e2057f948c78419955e9c98968cc53e5
kostok20 <81837358+kostok20@users.noreply.github.com>
2022-05-17 18:23:15 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
-TOKEN = "5250471059:AAEla-qXzMJQrrbK9D6FlaViH6nti8Tx2hg"
+TOKEN = "5353713878:AAF12ki6dgCZNV2qQHcDPPur4y3v40ABp5U"
 with open('words.txt', 'r', encoding='utf-8') as file:
     word_list = file.read().splitlines()
